32bb282b7599d7f97e4d9a43e3b8f88be584c092
hugofalkman <zilla@hs-macbook-pro.local>
2018-01-30 00:05:53 +0100

    Lecture 8 complete

--- PlayingCard/PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -194,6 +194,10 @@ extension CGPoint {
     }
 }
 
-
+extension CGFloat {
+    var arc4random: CGFloat {
+        return self * (CGFloat(arc4random_uniform(UInt32.max))/CGFloat(UInt32.max))
+    }
+}
 
 
